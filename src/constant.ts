bb4c6eea978c03e19cfed8e00b1a67de1ecb63eb
Lutz <meteo_meteo@naver.com>
2023-01-21 01:32:27 +0900

    feat: define tokenizer constant

---
+++ src/constant.ts
@@ -0,0 +1,36 @@
+export enum TokenType {
+  UnaryLogicalOperator = "UnaryLogicalOperator",
+  BinaryLogicalOperator = "BinaryLogicalOperator",
+  BooleanLiteral = "BooleanLiteral",
+  DateLiteral = "DateLiteral",
+  StringLiteral = "StringLiteral",
+  NumericLiteral = "NumericLiteral",
+  Identifier = "Identifier",
+  Comma = "Comma",
+  OpenParenthesis = "OpenParenthesis",
+  CloseParenthesis = "CloseParenthesis",
+  EqualityCompareOperator = "EqualityCompareOperator",
+  GreaterOrLesserCompareOperator = "GreaterOrLesserCompareOperator",
+  EndOfQuery = "EedOfQuery",
+}
+
+export enum UnaryLogicalOperator {
+  Not = "NOT",
+}
+
+export enum BinaryLogicalOperator {
+  And = "AND",
+  Or = "OR",
+}
+
+export enum EqualityCompareOperator {
+  Equal = "==",
+  NotEqual = "!=",
+}
+
+export enum GreaterOrLesserCompareOperator {
+  GreaterThan = ">",
+  GreaterOrEqualThan = ">=",
+  LesserThan = "<",
+  LesserOrEqualThan = "<=",
+}
